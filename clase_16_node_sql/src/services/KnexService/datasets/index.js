ebc41c7fa8edcb543a2e222804463937ac8c97b2
Alan <alansinicco@gmail.com>
2022-11-08 20:42:12 -0300

    ejemplo clase 16 sql

---
+++ clase_16_node_sql/src/services/KnexService/datasets/index.js
@@ -0,0 +1,43 @@
+const products = [
+  {
+    title: "mac m1",
+    price: "123",
+    thumbnail:
+      "https://www.apple.com/la/mac/why-mac/images/overview/hero__dg0dsnic3gia_large.png",
+  },
+  {
+    title: "mac m2",
+    price: "123",
+    thumbnail:
+      "https://www.apple.com/la/mac/why-mac/images/overview/hero__dg0dsnic3gia_large.png",
+  },
+  {
+    title: "mac m1 pro",
+    price: "123",
+    thumbnail:
+      "https://www.apple.com/la/mac/why-mac/images/overview/hero__dg0dsnic3gia_large.png",
+  },
+];
+
+const messages = [
+  {
+    id: 1,
+    email: "alan@jsx.com",
+    text: "Hola!!!! ",
+    timestamp: "15/7/2022 19:11:14",
+  },
+  {
+    id: 2,
+    email: "alan@jsx.com",
+    text: "¿Cómo están??",
+    timestamp: "15/7/2022 19:11:26",
+  },
+  {
+    id: 3,
+    email: "32140@jsx.com",
+    text: "Avanzandoooo todos juntos 😎",
+    timestamp: "15/7/2022 19:11:51",
+  },
+];
+
+export const DATASETS = { products, messages };
